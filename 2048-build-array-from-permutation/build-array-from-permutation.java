755b029e8cec60cb40940a2d89c8277d2c6c17c2
Anusha <127849784+anushaa006@users.noreply.github.com>
2024-05-19 17:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (81.64%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.java
+++ 2048-build-array-from-permutation/build-array-from-permutation.java
@@ -7,13 +7,13 @@ class Solution {
         return nums;
     }
     
-    void aPermutation(int[] nums,int start){
+    static void aPermutation(int[] nums,int start){
         if(start<nums.length){
-        int temp=nums[start];
-        int result=nums[temp];
-        aPermutation(nums,start+1);
-        nums[start]=result;
-    }
+            int temp=nums[start];
+            int result=nums[temp];
+            aPermutation(nums,start+1);
+            nums[start]=result;
+        }
   
     }
 }
